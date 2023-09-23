1a4f38ba8a81387f6e135effdc92e5d87cdd76f6
lucschwalm <lucschwalm03@gmail.com>
2023-09-22 22:04:04 -0500

    Added footer icons and changed formatting

---
+++ develop/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer () {
+    return(
+        <div className="d-flex justify-content-center pb-2">
+            <a className="mx-2 mb-2 text-dark" href="#">
+                <i className="fa-brands fa-github fa-2xl"></i>
+            </a>
+            <a className="mx-2 mb-2 text-dark" href="#">
+                <i className="fa-brands fa-linkedin fa-2xl"></i>
+            </a>
+        </div>
+    );
+}
