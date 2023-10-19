668d20e3a81dc700ba10abbbf67247fc922a375b
lucschwalm <lucschwalm03@gmail.com>
2023-10-19 18:30:30 -0500

    Added parkit project

--- develop/src/components/Portfolio.jsx
+++ develop/src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import Project from "./Project";
 import moviefinder from '../assets/moviefinder.PNG';
 import plantit from '../assets/plantit.PNG';
+import parkit from '../assets/parkit.PNG';
 
 export default function Portfolio() {
     return(
@@ -9,7 +10,7 @@ export default function Portfolio() {
             <div className="d-flex flex-wrap">
                 <Project image={moviefinder} projectName={"Moviefinder"} projectLanguage={"JS/HTML"} projectLink={"https://github.com/lucschwalm/moviefinder/tree/main"} />
                 <Project image={plantit} projectName={"PlantIt"} projectLanguage={"JS/Handlebars"} projectLink={"https://github.com/RossiniTheRat/plant-it/tree/main"} />
-                <Project image={"https://placehold.jp/430x306.png"} projectName={"Placeholder"} projectLanguage={"Coming soon"} projectLink={"/#"} />
+                <Project image={parkit} projectName={"ParkIt"} projectLanguage={"MERN Stack"} projectLink={"https://parkit-project-4482020d00b5.herokuapp.com/"} />
                 <Project image={"https://placehold.jp/430x306.png"} projectName={"Placeholder"} projectLanguage={"Coming soon"} projectLink={"/#"} />
                 <Project image={"https://placehold.jp/430x306.png"} projectName={"Placeholder"} projectLanguage={"Coming soon"} projectLink={"/#"} />
                 <Project image={"https://placehold.jp/430x306.png"} projectName={"Placeholder"} projectLanguage={"Coming soon"} projectLink={"/#"} />
